550c5c1c13cf35c667b1869f75229736e44252a0
sarafernandes506 <dmhsouza@outlook.com>
2025-05-07 11:55:43 -0300

    commit antes de atualizar o expo

--- backend/main.py
+++ backend/main.py
@@ -34,6 +34,6 @@ def read_root():
     return {"message": "Welcome to the API"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="192.168.1.2", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
